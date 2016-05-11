42a36050ac080424327066e57d26a2c1f4693130
Angelo Scozzoli <scozzoli@gmail.com>
2016-05-11 10:09:55 +0200

    Aggiunto stile "nohover" per le tabelle di tipo "fix" Aggiunta visualizzazione delle tabelle nella ricerca db_utility Aggiunta prima bozza della documentazione I18n

--- modules/system/Pi_DB_Utility/call/CercaMSSQL.php
+++ modules/system/Pi_DB_Utility/call/CercaMSSQL.php
@@ -21,6 +21,43 @@
 			and p.name != 'sysname'
 			ORDER BY schema_name, table_name";
 	
+	$qryTab = "SELECT t.name AS table_name,
+			SCHEMA_NAME(t.schema_id) AS schema_name,
+			count(c.name) as column_number,
+			t.object_id as object_id
+
+			FROM sys.tables AS t
+			INNER JOIN sys.columns c ON t.object_id = c.object_id
+			WHERE upper(t.name) LIKE '{$cerca}'
+			group by t.name, t.schema_id,t.object_id
+			ORDER BY schema_name, table_name";
+	
+	$resTab = $db->get($qryTab);
+	
+	$outTab = '<table class="lite red" data-pi-component="tablesort">
+		<tr>
+			<th>Tabella</th>
+			<th>Schema</th>
+			<th>Numero Colonne</th>
+		</tr>';
+	$idx = 0;
+	foreach($resTab as $k=>$v){
+		$outTab .= '<tr style="cursor:pointer" onclick="pi.request(\'tableObj'.$idx.'\',\'WinTableDettMSSQL\')">
+			<td id="tableObj'.$idx.'">
+				<input type="hidden" name="id" value="'.$v['object_id'].'">
+				<input type="hidden" name="db">
+				<input type="hidden" name="table">
+				<input type="hidden" name="schema">
+				'.$v['table_name'].'
+			</td>
+			<td>'.$v['schema_name'].'</td>
+			<td>'.$v['column_number'].'</td>
+		</tr>';
+		$pr->addFill('tableObj'.$idx, array('table' => $v['table_name'], 'schema' => $v['schema_name'], 'db' => $pr->post('db')));
+		$idx++;
+	}
+	$outTab.='</table>';
+	
 	$res = $db->get($qry,true);
 	$out = '<table class="lite blue" data-pi-component="tablesort">
 		<tr>
@@ -29,7 +66,6 @@
 			<th>Colonna</th>
 			<th>Tipo Dato</th>
 		</tr>';
-	$idx = 0;
 	$fill = array();
 	foreach($res as $k=>$v){
 		if($v['collation_name'] == $db->opt('null')){
@@ -53,5 +89,9 @@
 		$idx++;
 	}
 	$out .= '</table>';
-	$pr->addHtml('container',$out)->response();
+	
+	$content = '<div class="panel" data-pic="'.(count($resTab) > 0 ? 'collapse' : 'collapse : {close:true}').'"><div class="header red">Tabelle ('.count($resTab).')</div>'.$outTab.'</div>';
+	$content .= '<div class="panel" data-pic="'.(count($res) > 0 ? 'collapse' : 'collapse : {close:true}').'"><div class="header blue">Colonne ('.count($res).')</div>'.$out.'</div>';
+	
+	$pr->addHtml('container',$content)->response();
 ?>
